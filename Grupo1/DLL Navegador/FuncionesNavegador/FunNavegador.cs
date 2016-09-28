79b7a365adf1723435a5441e392de12bc9d13d6f
ottorm9 <ottoguate@hotmail.com>
2016-09-28 10:58:40 -0600

    Navegador-Insertar

---
+++ Grupo1/DLL Navegador/FuncionesNavegador/FunNavegador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FuncionesNavegador
+{
+    public class FunNavegador
+    {
+        public void activartextbox(TextBox textbox)
+        {
+            textbox.Enabled = true;
+        }
+
+        public void desactivartextbox(TextBox textbox)
+        {
+            textbox.Enabled = false;
+        }
+
+        public void insertar(DataTable datos, string tabla)
+        {
+            Conexionmysql.ObtenerConexion();
+            string query1 = "insert into " + tabla + " (";
+            string query2 = "values (";
+            int cuentaFilas = datos.Rows.Count;
+            DataRow contenido;
+            for (int fila = 0; fila < cuentaFilas; fila++)
+            {
+                contenido = datos.Rows[fila];
+                query1 = query1 + contenido["Columna"].ToString();
+                query2 = query2 + "'" + contenido["Valor"].ToString() + "'";
+                if (fila!=(cuentaFilas-1))
+                {
+                    query1 = query1 + ", ";
+                    query2 = query2 + ", ";
+                }
+            }
+            string query = query1 + ") " + query2 + ");";
+            Conexionmysql.EjecutarMySql(query);
+            Conexionmysql.Desconectar();
+        }
+    }
+}
---
+++ Grupo1/Navegador/Navegador/Componente.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FuncionesNavegador;
+
+namespace Navegador
+{
+    public partial class Componente : Form
+    {
+        public Componente()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {
+            FunNavegador obj = new FunNavegador();
+            obj.activartextbox(textBox1);
+            obj.activartextbox(textBox2);
+            obj.activartextbox(textBox3);
+        }
+
+        private void Componente_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            FunNavegador fn = new FunNavegador();
+            TextBox[] textbox = { textBox1, textBox2, textBox3 };
+            DataTable datos = construirDataTable(textbox);
+            if (datos.Rows.Count==0)
+            {
+                MessageBox.Show("Hay campos vacios", "Favor Verificar",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            string tabla = "empleado";
+            fn.insertar(datos,tabla);
+        }
+        private DataTable construirDataTable(TextBox[] textbox)
+        {
+            DataTable datos = new DataTable();
+            datos.Columns.Add("Columna", typeof(string));
+            datos.Columns.Add("Valor", typeof(string));
+            DataRow fila;
+            foreach (TextBox tb in textbox)
+            {
+                fila = datos.NewRow();
+                fila["Columna"] = tb.Tag.ToString();
+                fila["Valor"] = tb.Text;
+                if (tb.Text == "")
+                {
+                    datos.Clear();
+                    return datos;
+                }
+                datos.Rows.Add(fila);
+            }
+            return datos;
+        }
+    }
+}
