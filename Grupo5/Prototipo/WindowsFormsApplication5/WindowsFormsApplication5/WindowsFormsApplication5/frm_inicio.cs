25af413d63b66da5d73e266e63cf3a3037de7062
DCisneros <cdanielestrada@gmail.com>
2016-10-14 13:07:07 -0600

    Revert "Actualisacion"
    This reverts commit d30923fbfc31d95434ddd6c5e91cebf852a29bc5.

---
+++ Grupo5/Prototipo/WindowsFormsApplication5/WindowsFormsApplication5/WindowsFormsApplication5/frm_inicio.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class frm_inicio : Form
+    {
+        public frm_inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
